6eb57744cad28fffe3dccfc88fec9dee3fedff35
George <36856568+WebDevCode@users.noreply.github.com>
2025-09-20 05:16:21 +0000

    initial setup with nativewind and expo router

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'expo-router';
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg my-10">Hello World</Text>
+      <Link className="underline my-2" href="/sign-in">Sign In</Link>
+      <Link className="underline my-2" href="/explore">Explore</Link>
+      <Link className="underline my-2" href="/profile">Profile</Link>
+      <Link className="underline my-2" href="/properties/1">Property</Link>
+    </View>
+  );
+}
